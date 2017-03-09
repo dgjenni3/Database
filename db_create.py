9b73f893a4fce8c1864504d07ead4893eb94c7c7
nareshganduri <gandurinaresh@gmail.com>
2017-03-08 21:09:00 -0600

    added python script to destroy all database tables

--- db_create.py
+++ db_create.py
@@ -4,43 +4,59 @@ from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 import os.path
+# this python file creates all the database tables that will be used by the application
+# DO NOT RUN THIS FILE MORE THAN ONCE --- you only need to create the tables once!
+
+db.create_all()
 #create the SONG table
 sql_str = "CREATE TABLE Song(" + \
-"Title VARCHAR NOT NULL," + \ 
-"Created_at INT NOT NULL," + \ 
-"Soundcloud_Views INT NOT NULL," + \ 
-"Song_Url VARCHAR NOT NULL," + \ 
-"Genre VARCHAR NOT NULL," + \ 
-"Track_type VARCHAR NOT NULL," + \
-"Duration INT NOT NULL," + \
-"Soundcloud_Favorites INT NOT NULL," + \
-"PRIMARY KEY (Song_Url));"
+	"Title VARCHAR NOT NULL," + \
+	"Created_at INT NOT NULL," + \
+	"Soundcloud_Views INT NOT NULL," + \
+	"Song_Url VARCHAR NOT NULL," + \
+	"Genre VARCHAR NOT NULL," + \
+	"Track_type VARCHAR NOT NULL," + \
+	"Duration INT NOT NULL," + \
+	"Soundcloud_Favorites INT NOT NULL," + \
+	"PRIMARY KEY (Song_Url));"
 db.engine.execute(sql_str)
 
 #create the ARTIST table
 sql_str = "CREATE TABLE Artist(" + \
-"Name VARCHAR NOT NULL," + \
-"Artist_Url VARCHAR NOT NULL," + \
-"Number_Followers INT NOT NULL," + \
-"PRIMARY KEY (Artist_Url));"
+	"Name VARCHAR NOT NULL," + \
+	"Artist_Url VARCHAR NOT NULL," + \
+	"Number_Followers INT NOT NULL," + \
+	"PRIMARY KEY (Artist_Url));"
 db.engine.execute(sql_str)
 
 #create the USER table
 sql_str = "CREATE TABLE User(" + \
-"Username VARCHAR NOT NULL," + \
-"Password VARCHAR NOT NULL," + \
-"E-mail VARCHAR NOT NULL," + \
-"Artist_Url VARCHAR NOT NULL," + \
-"PRIMARY KEY (E-mail));"
+	"Username VARCHAR NOT NULL," + \
+	"Password VARCHAR NOT NULL," + \
+	"Email VARCHAR NOT NULL," + \
+	"Artist_Url VARCHAR NOT NULL," + \
+	"PRIMARY KEY (Email));"
 db.engine.execute(sql_str)
 
 #create the BY table
+sql_str = "CREATE TABLE By(" + \
+	"Album_Name VARCHAR NOT NULL," + \
+	"Artist_Url VARCHAR NOT NULL," + \
+	"Song_Url VARCHAR NOT NULL);"
 db.engine.execute(sql_str)
 
 #create the UPVOTE table
+sql_str = "CREATE TABLE Upvote(" + \
+	"Email VARCHAR NOT NULL," + \
+	"Song_Url VARCHAR NOT NULL);"
 db.engine.execute(sql_str)
 
 #create the PLAYLIST table
+sql_str = "CREATE TABLE Playlist(" + \
+	"Name VARCHAR NOT NULL," + \
+	"Email VARCHAR NOT NULL," + \
+	"Song_Url VARCHAR NOT NULL," + \
+	"PRIMARY KEY (Name));"
 db.engine.execute(sql_str)
 
 # commit the results to keep them
---
+++ db_destroy.py
@@ -0,0 +1,33 @@
+#!flask/bin/python
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+# this python file deletes all the database tables that will be used by the application
+# DO NOT RUN THIS FILE MORE THAN ONCE --- you only need to delete the tables once!
+# delete the SONG table
+db.engine.execute("DROP TABLE Song;")
+
+# delete the ARTIST table
+db.engine.execute("DROP TABLE Artist;")
+
+# delete the USER table
+db.engine.execute("DROP TABLE User;")
+
+# delete the BY table
+db.engine.execute("DROP TABLE By;")
+
+# delete the UPVOTE table
+db.engine.execute("DROP TABLE Upvote;")
+
+# delete the PLAYLIST table
+db.engine.execute("DROP TABLE Playlist;")
+
+# commit the results to keep them
+db.session.commit()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
