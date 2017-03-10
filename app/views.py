12563051e3c93d010156a9e6970d5dc846715ac1
nareshganduri <gandurinaresh@gmail.com>
2017-03-10 15:19:59 -0600

    updated delete function to properly handle GET requests

--- app/views.py
+++ app/views.py
@@ -84,8 +84,8 @@ def songs():
 	
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
+	error = False
 	if request.method == 'POST':
-		error = False
 		sql_str = "SELECT Song_Url FROM Song WHERE Song_Url='" + request.form['song_url'] + "';"
 		req_song = db.engine.execute(sql_str).fetchall()
 		if len(req_song) == 0:
@@ -95,4 +95,5 @@ def delete():
 		sql_str = "DELETE FROM Song WHERE Song_Url='" + request.form['song_url'] + "';"
 		del_song = db.engine.execute(sql_str)
 		return render_template("delete.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'], error=error)
+	return render_template("delete.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'], error=error)
 	
