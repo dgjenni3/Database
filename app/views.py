062dbeb6bd707b2cfee72684db26d2582d16feeb
nareshganduri <gandurinaresh@gmail.com>
2017-03-10 21:02:24 -0600

    updated files to avoid using Flask-SQLAlchemy - only standard sqlalchemy is being used now, and the database connection is being done manually at the start of __init__.py - modified db_create.py to handle a manually created MetaData object, which stores information about all tables currently in the database, and will check to see if tables exist/don't exist before creating/deleting them

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, session
-from app import app, db
+from app import app, conn
 import os, hashlib
 
 @app.before_request
@@ -18,10 +18,10 @@ def index():
 def login():
 	if request.method == 'POST':
 		sql_str = "SELECT Username FROM UserTable WHERE Username='" + request.form['username'] + "';"
-		req_user = db.engine.execute(sql_str).fetchall()
+		req_user = conn.execute(sql_str).fetchall()
 		hashed_pass = hashlib.sha256(request.form['password'] + "SALT").hexdigest()
 		sql_str = "SELECT Password FROM UserTable WHERE Password='" + hashed_pass + "';"
-		req_pass = db.engine.execute(sql_str).fetchall()
+		req_pass = conn.execute(sql_str).fetchall()
 		valid = False
 		if len(req_user) == 1 and len(req_pass) == 1:
 			if str(req_user[0][0]) == request.form['username']:
@@ -43,13 +43,13 @@ def signup():
 	error = False
 	if request.method == 'POST':
 		sql_str = "SELECT Email FROM UserTable WHERE Email='" + request.form['email'] + "';"
-		req_email = db.engine.execute(sql_str).fetchall()
+		req_email = conn.execute(sql_str).fetchall()
 		if len(req_email) != 0:
 			error = True
 		hashed_pass = hashlib.sha256(request.form['password'] + "SALT").hexdigest()
 		sql_str = "INSERT INTO UserTable(Username, Password, Email, Artist_Url) VALUES ('" + request.form['username'] + "', '" + \
 		hashed_pass + "', '" + request.form['email'] + "', '" + request.form['username'] + "');"
-		create_user = db.engine.execute(sql_str)
+		create_user = conn.execute(sql_str)
 		session['CURR_USER'] = request.form['username']
 		session['LOGGED_IN'] = 'YES'
 		if error == False:
@@ -72,27 +72,27 @@ def upload():
 		sql_str = "INSERT INTO Song(Title, Created_at, Soundcloud_Views, Song_Url, Genre, Track_type, Duration, " + \
 		"Soundcloud_Favorites) VALUES('" + request.form['title'] + "', 1234, 0, '" + request.form['song_url'] + \
 		"', '" + request.form['genre'] + "', '" + request.form['track_type'] + "', 8, 0);"
-		new_song = db.engine.execute(sql_str)
+		new_song = conn.execute(sql_str)
 		return redirect(url_for('songs'))
 	return render_template("upload.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'])
 	
 @app.route('/songs')
 def songs():
 	sql_str = "SELECT * FROM Song;"
-	all_songs = db.engine.execute(sql_str).fetchall()
+	all_songs = conn.execute(sql_str).fetchall()
 	return render_template("songs.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'], song_list=all_songs)
 	
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
 	if request.method == 'POST':
 		sql_str = "SELECT Song_Url FROM Song WHERE Song_Url='" + request.form['song_url'] + "';"
-		req_song = db.engine.execute(sql_str).fetchall()
+		req_song = conn.execute(sql_str).fetchall()
 		if len(req_song) == 0:
 			error = True
 			return render_template("delete.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'], error='True')
 		
 		sql_str = "DELETE FROM Song WHERE Song_Url='" + request.form['song_url'] + "';"
-		del_song = db.engine.execute(sql_str)
+		del_song = conn.execute(sql_str)
 		return render_template("delete.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'], error='False')
 	return render_template("delete.html", username=session['CURR_USER'], logged_in=session['LOGGED_IN'])
 	
@@ -101,11 +101,11 @@ def song_edit(song_url):
 	if request.method == 'POST':
 		sql_str = "UPDATE Song SET Title='" + request.form['title'] + "', " + "Genre='" + request.form['genre'] + \
 		"', Track_type='" + request.form['track_type'] + "' WHERE Song_Url='" + song_url + "';"
-		updated_song = db.engine.execute(sql_str)
+		updated_song = conn.execute(sql_str)
 		return render_template("edit.html", error='False')
 	elif request.method == 'GET':
 		sql_str = "SELECT * FROM Song WHERE Song_Url='" + song_url + "';"
-		req_song = db.engine.execute(sql_str).fetchall()
+		req_song = conn.execute(sql_str).fetchall()
 		song_title = req_song[0][0]
 		genre = req_song[0][4]
 		track_type = req_song[0][5]
